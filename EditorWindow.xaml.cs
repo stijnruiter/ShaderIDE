e2ba50aeeb66a142165d819b8e703e7864db8ff5
stijnruiter <stijnruiter@hotmail.com>
2025-01-27 23:00:48 +0100

    Crude syntax highlighting

--- EditorWindow.xaml.cs
+++ EditorWindow.xaml.cs
@@ -2,6 +2,9 @@
 using ShaderIDE.Render;
 using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace ShaderIDE;
 
@@ -15,7 +18,7 @@ public partial class EditorWindow : Window
             MajorVersion = 3,
             MinorVersion = 3
         });
-        TextBoxFragmentShader.Text = Shader.DefaultFragmentShader;
+        RichTextBoxFragmentShader.AppendText(Shader.DefaultFragmentShader);
         _canvas = new RenderCanvas();
     }
 
@@ -27,7 +30,7 @@ public partial class EditorWindow : Window
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        _canvas.UpdateShader(Shader.DefaultVertexShader, TextBoxFragmentShader.Text);
+        _canvas.UpdateShader(Shader.DefaultVertexShader, TextRange(RichTextBoxFragmentShader).Text);
     }
 
     private void Window_Closed(object sender, EventArgs e)
@@ -35,5 +38,63 @@ public partial class EditorWindow : Window
         _canvas.Dispose();
     }
 
-    private RenderCanvas _canvas;
+    private readonly RenderCanvas _canvas;
+
+    private bool ignoreTextChanges = false;
+
+    private void RichTextBoxFragmentShader_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (ignoreTextChanges)
+            return;
+
+        ignoreTextChanges = true;
+        RichTextBoxFragmentShader.BeginChange();
+        try
+        {
+            HighlightXml(RichTextBoxFragmentShader);
+        }
+        finally
+        {
+            RichTextBoxFragmentShader.EndChange();
+            ignoreTextChanges = false;
+        }
+    }
+
+    private static TextRange TextRange(RichTextBox richTextBox)
+    {
+        return new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+    }
+
+    public static void HighlightXml(RichTextBox richTextBox)
+    {
+        var newDocument = richTextBox.Document;
+        var range = new TextRange(newDocument.ContentStart, newDocument.ContentEnd);
+        range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Black));
+
+        var position = newDocument.ContentStart;
+        while(position != null && position.CompareTo(newDocument.ContentEnd) <= 0)
+        {
+            if (position.CompareTo(newDocument.ContentEnd) == 0)
+                return;
+
+            var textRun = position.GetTextInRun(LogicalDirection.Forward);
+            foreach(var search in KeyWords)
+            {
+                var indexInRun = textRun.IndexOf(search, StringComparison.CurrentCulture);
+                if (indexInRun >= 0)
+                {
+                    var keywordPos = position.GetPositionAtOffset(indexInRun, LogicalDirection.Forward);
+                    if (keywordPos != null)
+                    {
+                        var nextPointer = keywordPos.GetPositionAtOffset(search.Length);
+                        var textRange = new TextRange(keywordPos, nextPointer);
+                        textRange.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Red));
+                    }
+                }
+            }
+            position = position!.GetNextContextPosition(LogicalDirection.Forward);
+        }
+    }
+
+    private static string[] KeyWords = { "void", "out"};
 }
