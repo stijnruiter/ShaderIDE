29f353de126962837a6583a31475f753d0ac294a
stijnruiter <stijnruiter@hotmail.com>
2025-01-30 01:00:54 +0100

    Move syntax highlighting code to a custom wpf control

---
+++ Controls/SyntaxTextBox.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace ShaderIDE.Controls;
+
+public class SyntaxTextBox : RichTextBox
+{
+    public SyntaxTextBox()
+    {
+        AcceptsTab = true;
+        AcceptsReturn = true;
+        Document.LineHeight = 1;
+        FontFamily = new FontFamily("Consolas");
+        TextChanged += SyntaxTextBox_TextChanged;
+    }
+
+    private void SyntaxTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            TextChanged -= SyntaxTextBox_TextChanged;
+            BeginChange();
+            ApplySyntaxHighlighting();
+        }
+        finally
+        {
+            EndChange();
+            TextChanged += SyntaxTextBox_TextChanged;
+        }
+    }
+
+    public TextRange TextRange => new(Document.ContentStart, Document.ContentEnd);
+
+    public string Text
+    {
+        get => TextRange.Text;
+        set => TextRange.Text = value;
+    }
+
+    private readonly SolidColorBrush ColorBlack = new(Colors.Black);
+    private readonly SolidColorBrush ColorBlue = new(Colors.Blue);
+    private readonly SolidColorBrush ColorRed = new(Colors.Red);
+
+    private void ApplySyntaxHighlighting()
+    {
+        TextRange textBoxRange = TextRange;
+        textBoxRange.ApplyPropertyValue(TextElement.ForegroundProperty, ColorBlack);
+
+        var text = textBoxRange.Text;
+        TextPointer startPointer = textBoxRange.Start;
+        int offset = 0;
+        string token = string.Empty;
+
+        for (var index = 0; index < text.Length; index++)
+        {
+            char nextChar = text[index];
+            if (char.IsLetterOrDigit(nextChar))
+            {
+                token += nextChar;
+                continue;
+            }
+
+            if (GetTokenColor(token) is { } color && GetTextRangeOfToken(startPointer, offset, token.Length) is { } tokenRange)
+            {
+                tokenRange.ApplyPropertyValue(TextElement.ForegroundProperty, color);
+                startPointer = tokenRange.Start;
+                offset = 0;
+            }
+
+            offset += token.Length;
+            token = string.Empty;
+
+            if (!Environment.NewLine.Contains(nextChar))
+            {
+                offset++;
+            }
+        }
+    }
+
+    private SolidColorBrush? GetTokenColor(string token)
+    {
+        if (KeyWords.Any(s => s.Equals(token, StringComparison.InvariantCultureIgnoreCase)))
+            return ColorBlue;
+
+        if (DataTypes.Any(s => s.Equals(token, StringComparison.InvariantCultureIgnoreCase)))
+            return ColorRed;
+
+        return null;
+    }
+
+    private static TextRange GetTextRangeOfToken(TextPointer startPointer, int offset, int characterCount)
+    {
+        var start = GetTextPositionAtOffset(startPointer, offset);
+        if (start is null)
+        {
+            Debug.Fail("Start TextPointer not found.");
+            return null;
+        }
+        var end = GetTextPositionAtOffset(start, characterCount);
+        if (start is null)
+        {
+            Debug.Fail("End TextPointer not found.");
+            return null;
+        }
+        return new TextRange(start, end);
+    }
+
+    private static TextPointer? GetTextPositionAtOffset(TextPointer position, int characterCount)
+    {
+        while (position != null)
+        {
+            if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+            {
+                int count = position.GetTextRunLength(LogicalDirection.Forward);
+                if (characterCount <= count)
+                {
+                    return position.GetPositionAtOffset(characterCount);
+                }
+
+                characterCount -= count;
+            }
+
+            TextPointer nextContextPosition = position.GetNextContextPosition(LogicalDirection.Forward);
+            if (nextContextPosition == null)
+                return position;
+
+            position = nextContextPosition;
+        }
+
+        return position;
+    }
+
+    private static string[] KeyWords = { "void", "out" };
+    private static string[] DataTypes = { "vec3", "vec4", "int" };
+}
--- EditorWindow.xaml.cs
+++ EditorWindow.xaml.cs
@@ -1,12 +1,7 @@
 ﻿using OpenTK.Wpf;
 using ShaderIDE.Render;
 using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Media;
 
 namespace ShaderIDE;
 
@@ -20,7 +15,7 @@ public partial class EditorWindow : Window
             MajorVersion = 3,
             MinorVersion = 3
         });
-        RichTextBoxFragmentShader.AppendText(Shader.DefaultFragmentShader);
+        fragmentShaderTextBox.Text = Shader.DefaultFragmentShader;
         _canvas = new RenderCanvas();
     }
 
@@ -32,8 +27,7 @@ public partial class EditorWindow : Window
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        var content = new TextRange(RichTextBoxFragmentShader.Document.ContentStart, RichTextBoxFragmentShader.Document.ContentEnd);
-        _canvas.UpdateShader(Shader.DefaultVertexShader, content.Text);
+        _canvas.UpdateShader(Shader.DefaultVertexShader, fragmentShaderTextBox.Text);
     }
 
     private void Window_Closed(object sender, EventArgs e)
@@ -41,111 +35,5 @@ public partial class EditorWindow : Window
         _canvas.Dispose();
     }
 
-    private void RichTextBoxFragmentShader_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        try
-        {
-            RichTextBoxFragmentShader.TextChanged -= RichTextBoxFragmentShader_TextChanged;
-            RichTextBoxFragmentShader.BeginChange();
-            ApplySyntaxHighlighting(RichTextBoxFragmentShader);
-        }
-        finally
-        {
-            RichTextBoxFragmentShader.EndChange();
-            RichTextBoxFragmentShader.TextChanged += RichTextBoxFragmentShader_TextChanged;
-        }
-    }
-
-    private void ApplySyntaxHighlighting(RichTextBox richTextBox)
-    {
-        TextRange textBoxRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
-        textBoxRange.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Black));
-
-        var text = textBoxRange.Text;
-        int offset = 0;
-        string token = string.Empty;
-        TextPointer startPointer = textBoxRange.Start;
-        for (var index = 0; index < text.Length; index++)
-        {
-            char nextChar = text[index];
-            if (char.IsLetterOrDigit(nextChar))
-            {
-                token += nextChar;
-                continue;
-            }
-
-            if (GetTokenColor(token) is { } color && GetTextRangeOfToken(startPointer, offset, token.Length) is { } tokenRange)
-            {
-                tokenRange.ApplyPropertyValue(TextElement.ForegroundProperty, color);
-                startPointer = tokenRange.Start;
-                offset = 0;
-            }
-                
-            offset += token.Length;
-            token = string.Empty;
-
-            if (!Environment.NewLine.Contains(nextChar))
-            {
-                offset++;
-            }
-        }
-    }
-
-    private SolidColorBrush? GetTokenColor(string token)
-    {
-        if (KeyWords.Any(s => s.Equals(token, StringComparison.InvariantCultureIgnoreCase)))
-            return new SolidColorBrush(Colors.Blue);
-        
-        if (DataTypes.Any(s => s.Equals(token, StringComparison.InvariantCultureIgnoreCase)))
-            return new SolidColorBrush(Colors.Red);
-        
-        return null;
-    }
-
-    private static TextRange GetTextRangeOfToken(TextPointer startPointer, int offset, int characterCount)
-    {
-        var start = GetTextPositionAtOffset(startPointer, offset);
-        if (start is null)
-        {
-            Debug.Fail("Start TextPointer not found.");
-            return null;
-        }
-        var end = GetTextPositionAtOffset(start, characterCount);
-        if (start is null)
-        {
-            Debug.Fail("End TextPointer not found.");
-            return null;
-        }
-        return new TextRange(start, end);
-    }
-
-    private static TextPointer? GetTextPositionAtOffset(TextPointer position, int characterCount)
-    {
-        while (position != null)
-        {
-            if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
-            {
-                int count = position.GetTextRunLength(LogicalDirection.Forward);
-                if (characterCount <= count)
-                {
-                    return position.GetPositionAtOffset(characterCount);
-                }
-
-                characterCount -= count;
-            }
-
-            TextPointer nextContextPosition = position.GetNextContextPosition(LogicalDirection.Forward);
-            if (nextContextPosition == null)
-                return position;
-
-            position = nextContextPosition;
-        }
-
-        return position;
-    }
-
-
     private readonly RenderCanvas _canvas;
-    private static string[] KeyWords = { "void", "out"};
-    private static string[] DataTypes = { "vec3", "vec4", "int"};
 }
