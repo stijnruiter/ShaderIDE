5b69c5f5df3fe4929247fa6ac6c625cb1c92d30b
stijnruiter <stijnruiter@hotmail.com>
2025-01-31 20:00:37 +0100

    Implement suggention box

--- Controls/SyntaxTextBox.cs
+++ Controls/SyntaxTextBox.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ShaderIDE.Controls;
@@ -11,11 +15,24 @@ public class SyntaxTextBox : RichTextBox
 {
     public SyntaxTextBox()
     {
+        SuggestionBox = new Popup()
+        {
+            PlacementTarget = this,
+            Placement = PlacementMode.Bottom,
+            StaysOpen = false,
+            Child = SuggestionItemList,
+            Focusable = false
+        };
+
         AcceptsTab = true;
         AcceptsReturn = true;
         Document.LineHeight = 1;
         FontFamily = new FontFamily("Consolas");
         TextChanged += SyntaxTextBox_TextChanged;
+        PreviewKeyUp += SyntaxTextBox_KeyUp;
+        KeyUp += SyntaxTextBox_KeyUp;
+        PreviewKeyDown += SyntaxTextBox_PreviewKeyDown;
+        SuggestionItemList.KeyDown += SuggestionItemList_KeyDown;
     }
 
     public TextRange TextRange => new(Document.ContentStart, Document.ContentEnd);
@@ -33,6 +50,7 @@ public class SyntaxTextBox : RichTextBox
             TextChanged -= SyntaxTextBox_TextChanged;
             BeginChange();
             ApplySyntaxHighlighting();
+            UpdateSuggestionBoxPosition();
         }
         finally
         {
@@ -40,6 +58,90 @@ public class SyntaxTextBox : RichTextBox
             TextChanged += SyntaxTextBox_TextChanged;
         }
     }
+
+    private void SyntaxTextBox_KeyUp(object sender, KeyEventArgs e)
+    {
+        if ((e.Key == Key.Space && e.ModifierPressed(ModifierKeys.Control)) ||
+            (e.Key == Key.LeftCtrl && e.KeyboardDevice.IsKeyDown(Key.Space)) ||
+            e.Key.IsAlphaNumeric())
+        {
+            ShowSuggestionBox();
+            return;
+        }
+
+        if (!SuggestionBox.IsOpen)
+            return;
+
+        switch (e.Key)
+        {
+            case Key.Up:
+                SuggestionItemList.SelectedIndex = (SuggestionItemList.Items.Count + SuggestionItemList.SelectedIndex - 1) % SuggestionItemList.Items.Count;
+                e.Handled = true;
+                return;
+            case Key.Down:
+                SuggestionItemList.SelectedIndex = (SuggestionItemList.SelectedIndex + 1) % SuggestionItemList.Items.Count;
+                e.Handled = true;
+                return;
+            case Key.Left:
+            case Key.Right:
+            case Key.Escape:
+                ClosePopup();
+                return;
+            case Key.Enter:
+            case Key.Tab:
+                ApplySuggestion((string)(SuggestionItemList.SelectedItem ?? SuggestionItemList.Items[0]));
+                e.Handled = true;
+                return;
+        };
+    }
+
+
+    private void SuggestionItemList_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (!SuggestionBox.IsOpen)
+            return;
+
+        switch(e.Key)
+        {
+            case Key.Tab:
+            case Key.Enter:
+                ApplySuggestion((string)SuggestionItemList.SelectedItem);
+                ClosePopup();
+                e.Handled = true;
+                return;
+        }
+    }
+
+    private void SyntaxTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (!SuggestionBox.IsOpen)
+            return;
+
+
+        switch (e.Key)
+        {
+            case Key.Enter:
+            case Key.Tab:
+            case Key.Up:
+            case Key.Down:
+                e.Handled = true;
+                return;
+        }
+    }
+
+    private void ApplySuggestion(string suggestion)
+    {
+        ClosePopup();
+
+        var range = GetWordRange(CaretPosition);
+        if (range is null)
+            return;
+           
+        range.Text = suggestion;
+        CaretPosition = range.End;
+        Focus();
+    }
+
 
     private void ApplySyntaxHighlighting()
     {
@@ -104,7 +206,7 @@ public class SyntaxTextBox : RichTextBox
         }
         return new TextRange(start, end);
     }
-
+    
     private static TextPointer? GetTextPositionAtOffset(TextPointer position, int characterCount)
     {
         while (position != null)
@@ -130,10 +232,111 @@ public class SyntaxTextBox : RichTextBox
         return position;
     }
 
+    public void ClosePopup()
+    {
+        SuggestionBox.IsOpen = false;
+        SuggestionItemList.SelectedIndex = 0;
+    }
+
+    public void ShowSuggestionBox()
+    {
+        UpdateSuggestionBoxPosition();
+        SuggestionBox.IsOpen = !SuggestionBox.PlacementRectangle.IsEmpty;
+    }
+
+    public void UpdateSuggestionBoxPosition()
+    {
+        SuggestionBox.PlacementRectangle = ComputePopupPlacement(CaretPosition);
+    }
+
+    private static Rect ComputePopupPlacement(TextPointer caret) 
+        => GetWordRange(caret)?.Start?.GetCharacterRect(LogicalDirection.Backward) ?? Rect.Empty;
+
+    private static TextPointer Clamp(TextPointer pointer, TextPointer left, TextPointer right)
+    {
+        Debug.Assert(left.CompareTo(right) <= 0);
+
+        if (pointer.CompareTo(left) < 0)
+            return left;
+
+        if (pointer.CompareTo(right) > 0)
+            return pointer;
+        
+        return pointer;
+    }
+
+    private static TextRange? GetWordRange(TextPointer caret)
+    {
+        if (caret is null) 
+            return null;
+
+        var lineStart = caret.GetLineStartPosition(0) ?? caret;
+        var lineEnd = caret.GetLineStartPosition(1) ?? caret;
+        var leftPointer = GetTextPointerOfLastAlphaNumeric(caret, LogicalDirection.Backward) ?? caret;
+        var rightPointer = GetTextPointerOfLastAlphaNumeric(caret, LogicalDirection.Forward) ?? caret;
+
+        leftPointer  = Clamp(leftPointer,  lineStart, lineEnd);
+        rightPointer = Clamp(rightPointer, lineStart, lineEnd);
+
+        // For some reason, it sometimes happens that there is still a non-alphanumeric character..
+        var match = Regex.Match(new TextRange(leftPointer, caret).Text, "[^a-zA-Z0-9]", RegexOptions.RightToLeft);
+        if (match.Success)
+        {
+            leftPointer = leftPointer.GetPositionAtOffset(match.Index) ?? leftPointer;
+        }
+
+         match = Regex.Match(new TextRange(caret, rightPointer).Text, "[^a-zA-Z0-9]");
+        if (match.Success)
+        {
+            rightPointer = caret.GetPositionAtOffset(match.Index) ?? rightPointer;
+        }
+
+        return new TextRange(leftPointer, rightPointer);
+    }
+
+    private static TextPointer? GetTextPointerOfLastAlphaNumeric(TextPointer caret, LogicalDirection direction)
+    {
+        if (caret is null)
+            return null;
+
+        char[] textbuffer = new char[1];
+        do
+        {
+            caret.GetTextInRun(direction, textbuffer, 0, 1);
+            caret = caret.GetNextInsertionPosition(direction);
+        }
+        while ((char.IsLetterOrDigit(textbuffer[0]) || textbuffer[0] == '\0') && caret != null);
+        return caret?.GetNextInsertionPosition(direction.Reversed());
+    }
+
+    private readonly Popup SuggestionBox;
+    private readonly ListBox SuggestionItemList = new ListBox()
+    {
+        Foreground = Brushes.Black,
+        ItemsSource = _keyWords.Concat(_dataTypes),
+        Focusable = false
+    };
+
     private static readonly SolidColorBrush _colorBlack = new(Colors.Black);
     private static readonly SolidColorBrush _colorBlue = new(Colors.Blue);
     private static readonly SolidColorBrush _colorRed = new(Colors.Red);
 
     private static readonly string[] _keyWords = { "void", "out" };
     private static readonly string[] _dataTypes = { "vec3", "vec4", "int" };
+}
+
+public static class KeyEventExtension
+{
+    public static bool ModifierPressed(this KeyboardEventArgs args, ModifierKeys modifierKey) 
+        => (args.KeyboardDevice.Modifiers & modifierKey) == modifierKey;
+
+    public static LogicalDirection Reversed(this LogicalDirection direction)
+        => direction == LogicalDirection.Backward ? LogicalDirection.Forward : LogicalDirection.Backward;
+
+    public static bool IsAlphaNumeric(this Key key)
+    {
+        int keyValue = (int)key;
+        return ((keyValue >= (int)Key.D0 && keyValue <= (int)Key.Z) || 
+            (keyValue >= (int)Key.NumPad0 && keyValue <= (int)Key.NumPad9)); 
+    }
 }
